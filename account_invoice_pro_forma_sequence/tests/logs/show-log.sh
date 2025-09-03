7362bdc2e8332198919d03593507e0861fd83554
zeroincombenze <info@shs-av.com>
2025-09-03 12:18:34 +0200

    [IMP] No more logs

--- account_invoice_pro_forma_sequence/tests/logs/show-log.sh
+++
@@ -1,1 +0,0 @@
-less -R $(readlink -f $(dirname $0))/account_invoice_pro_forma_sequence_20230131.txt
--- account_payment_term_extension/tests/logs/show-log.sh
+++
@@ -1,1 +0,0 @@
-less -R $(readlink -f $(dirname $0))/account_payment_term_extension_20240614.txt
